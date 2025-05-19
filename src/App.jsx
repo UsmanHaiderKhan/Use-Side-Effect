b4ff82d052750371b0814fe82e923a6093d9028b
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2025-05-19 18:43:29 +0200

    ♻️ Update : Add useCallBack function to avoid call function again and again

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
@@ -51,18 +51,19 @@ function App() {
     }
   }
 
-  function handleRemovePlace() {
+  const handleRemovePlace = useCallback(function handleRemovePlace() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
+    setIsModalOpen(false);
     const storeIds = JSON.parse(localStorage.getItem('pickedPlaces')) || [];
     const updatedIds = storeIds.filter((id) => id !== selectedPlace.current);
     localStorage.setItem('pickedPlaces', JSON.stringify(updatedIds));
-    setIsModalOpen(false);
+
     // const storeIds = JSON.parse(localStorage.getItem('pickedPlaces')) || [];
     // localStorage.setItem('pickedPlaces', JSON.stringify(storeIds.filter((place) => place.id !== selectedPlace.current)));
 
-  }
+  }, []);
 
   return (
     <>
